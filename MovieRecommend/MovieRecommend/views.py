dd452eb01b0769742c205b5ee38311819af5ef23
JakubD02 <114111975+JakubD02@users.noreply.github.com>
2024-04-21 13:15:15 +0200

    Add form handling for user input and update Bootstrap in home.html
    Include creation of forms.py and update Bootstrap styles in home.html for user input handling

--- MovieRecommend/MovieRecommend/views.py
+++ MovieRecommend/MovieRecommend/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .forms import LanguageGenreForm
 
 def home(request):
-    return render(request, 'home.html')
-    # return render(request, 'home.html', {})
+    if request.method == 'POST':
+        form = LanguageGenreForm(request.POST)
+        if form.is_valid():
+            selected_language = form.cleaned_data['language']
+            selected_genre = form.cleaned_data['genre']
+            description = form.cleaned_data['description']
+    else:
+        form = LanguageGenreForm()
+
+    return render(request, 'home.html', {'form': form})
